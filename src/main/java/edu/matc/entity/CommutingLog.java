26079809f7c779f081a180f27a6c352999d492de
Micah Darling <darlingmicah@gmail.com>
2025-03-11 17:33:54 -0500

    added commutinglog entity for commuting_logs database

---
+++ src/main/java/edu/matc/entity/CommutingLog.java
@@ -0,0 +1,78 @@
+package edu.matc.entity;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "commute_logs")
+public class CommutingLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "commute_type")
+    private String commuteType;
+
+    @Column(name = "time_spent")
+    private double timeSpent;
+
+    @Column(name = "cost")
+    private double cost;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCommuteType() {
+        return commuteType;
+    }
+
+    public void setCommuteType(String commuteType) {
+        this.commuteType = commuteType;
+    }
+
+    public double getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(double timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
---
+++ src/main/java/edu/matc/entity/CostProjection.java
@@ -0,0 +1,10 @@
+package edu.matc.entity;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="commuter_logs")
+
+public class CostProjection {
+
+}

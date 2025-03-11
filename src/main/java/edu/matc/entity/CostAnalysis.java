c9f1a844f127fdf628cd606941c1b48855a926db
Micah Darling <darlingmicah@gmail.com>
2025-03-11 17:58:43 -0500

    added costalsysis and costprojection entities

---
+++ src/main/java/edu/matc/entity/CostAnalysis.java
@@ -0,0 +1,102 @@
+package edu.matc.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cost_analyses")
+public class CostAnalysis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "analysis_id")
+    private int analysisId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user; // Relationship to the User entity
+
+    @Column(name = "commute_type" )
+    private String commuteType; // Type of transportation (Car, Bike, Walk, Public Transit, etc.)
+
+    @Column(name = "one_year_cost" )
+    private double oneYearCost; // 1-year cost projection
+
+    @Column(name = "two_year_cost" )
+    private double twoYearCost; // 2-year cost projection
+
+    @Column(name = "five_year_cost" )
+    private double fiveYearCost; // 5-year cost projection
+
+    @Column(name = "total_cost" )
+    private double totalCost; // Total projected cost across all time periods
+
+    // Getters and Setters
+    public int getAnalysisId() {
+        return analysisId;
+    }
+
+    public void setAnalysisId(int analysisId) {
+        this.analysisId = analysisId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getCommuteType() {
+        return commuteType;
+    }
+
+    public void setCommuteType(String commuteType) {
+        this.commuteType = commuteType;
+    }
+
+    public double getOneYearCost() {
+        return oneYearCost;
+    }
+
+    public void setOneYearCost(double oneYearCost) {
+        this.oneYearCost = oneYearCost;
+    }
+
+    public double getTwoYearCost() {
+        return twoYearCost;
+    }
+
+    public void setTwoYearCost(double twoYearCost) {
+        this.twoYearCost = twoYearCost;
+    }
+
+    public double getFiveYearCost() {
+        return fiveYearCost;
+    }
+
+    public void setFiveYearCost(double fiveYearCost) {
+        this.fiveYearCost = fiveYearCost;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    @Override
+    public String toString() {
+        return "CostAnalysis{" +
+                "analysisId=" + analysisId +
+                ", user=" + user.getId() + // Assuming User entity has getId() method
+                ", commuteType='" + commuteType + '\'' +
+                ", oneYearCost=" + oneYearCost +
+                ", twoYearCost=" + twoYearCost +
+                ", fiveYearCost=" + fiveYearCost +
+                ", totalCost=" + totalCost +
+                '}';
+    }
+}
--- src/main/java/edu/matc/entity/CostProjection.java
+++ src/main/java/edu/matc/entity/CostProjection.java
@@ -1,10 +1,80 @@
 package edu.matc.entity;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="commuter_logs")
-
+@Table(name = "cost_projections")
 public class CostProjection {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_id")
+    private int logId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id"  )
+    private User user; // Relationship to the User entity
+
+    @Column(name = "transportation_mode"  )
+    private String transportationMode; // Type of transportation (Car, Bike, Walk, etc.)
+
+    @Column(name = "date_added"  )
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateAdded; // The date this projection was created
+
+    @Column(name = "duration_in_minutes"  )
+    private int durationInMinutes; // Duration of commute in minutes
+
+    @Column(name = "distance_in_miles"  )
+    private double distanceInMiles; // Distance of commute in miles
+
+    // Getters and Setters
+    public int getLogId() {
+        return logId;
+    }
+
+    public void setLogId(int logId) {
+        this.logId = logId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTransportationMode() {
+        return transportationMode;
+    }
+
+    public void setTransportationMode(String transportationMode) {
+        this.transportationMode = transportationMode;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public double getDistanceInMiles() {
+        return distanceInMiles;
+    }
+
+    public void setDistanceInMiles(double distanceInMiles) {
+        this.distanceInMiles = distanceInMiles;
+    }
 }
